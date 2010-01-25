872333d76a712e261f0c8b564354f8237e8f4a24
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-01-25 09:44:52 +0000

    documentation.

--- trunk/src/net/sf/jncu/protocol/v2_0/DockingEventCommands.java
+++ trunk/src/net/sf/jncu/protocol/v2_0/DockingEventCommands.java
@@ -1,7 +1,8 @@
 package net.sf.jncu.protocol.v2_0;
 
 /**
- * Newton Docking Protocol event commands for 2.0 Newton ROM Extensions.<br>
+ * <h2>Newton Docking Protocol</h2> Newton Docking Protocol event commands for
+ * 2.0 Newton ROM Extensions.<br>
  * Newton communicates with the desktop by exchanging Newton event commands.
  * <p>
  * In the commands below, all data is padded with nulls to 4 byte boundaries.
@@ -20,6 +21,13 @@ package net.sf.jncu.protocol.v2_0;
  * length   // the length of the following command
  * data     // data, if any
  * </pre>
+ * 
+ * <h2>Compatibility</h2> The protocol described above for 2.0 is a superset of
+ * the 1.0 protocol. However, the protocol version has been incremented so old
+ * versions of Newton Connection will no longer work with this version. The
+ * reason for this is that, although the protocol itself is upwardly compatible,
+ * the data structures in other parts of the 2.0 newton have changed to such a
+ * degree that old versions of Newton Connection will no longer work.
  * 
  * @author moshew
  */
