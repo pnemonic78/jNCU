d7ff8fd25df172816486f9084242f473f5f17c91
mwaisberg <pnemonic78@users.noreply.github.com>
2010-01-25 16:03:27 +0000

    new.

---
+++ src/net/sf/jncu/protocol/v2_0/file/DockCommandFile.java
@@ -0,0 +1,14 @@
+package net.sf.jncu.protocol.v2_0.file;
+
+import net.sf.jncu.protocol.v2_0.DockingEventCommands;
+
+public class DockCommandFile extends DockingEventCommands {
+
+	/** Desktop to Newton. */
+	public static final class DesktopToNewton {
+	}
+
+	/** Newton to Desktop. */
+	public static final class NewtonToDesktop {
+	}
+}
