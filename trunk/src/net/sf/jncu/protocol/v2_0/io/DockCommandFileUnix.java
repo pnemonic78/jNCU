b6fe02d957e65dc2cb5d3989021e7d272e6ca0e8
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-01-26 16:43:48 +0000

    refactored.

---
+++ trunk/src/net/sf/jncu/protocol/v2_0/io/DockCommandFileUnix.java
@@ -0,0 +1,13 @@
+package net.sf.jncu.protocol.v2_0.io;
+
+public class DockCommandFileUnix {
+
+	/** Desktop to Newton. */
+	public static class DesktopToNewton extends DockCommandFile.DesktopToNewton {
+	}
+
+	/** Newton to Desktop. */
+	public static class NewtonToDesktop extends DockCommandFile.NewtonToDesktop {
+	}
+
+}
