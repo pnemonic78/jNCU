51b35c48f219c06cc002fe676010b24fb501fa24
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-02-01 17:12:57 +0000

    some frame receive.

--- trunk/src/net/sf/jncu/protocol/DockingFrame.java
+++ trunk/src/net/sf/jncu/protocol/DockingFrame.java
@@ -1,17 +1,56 @@
 package net.sf.jncu.protocol;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import net.sf.lang.ControlCharacter;
 
 /**
- * Docking frame constants.
+ * Docking frame.
  * 
  * @author moshew
  */
 public class DockingFrame {
 
-	/** Frame start. */
-	public static final byte[] FRAME_START = { ControlCharacter.SYN, ControlCharacter.DLE, ControlCharacter.STX };
-	/** Frame end. */
-	public static final byte[] FRAME_END = { ControlCharacter.DLE, ControlCharacter.ETX };
+	/** Frame preamble delimiter. */
+	public static final byte[] FRAME_PREAMBLE = { ControlCharacter.SYN, ControlCharacter.DLE, ControlCharacter.STX };
+	/** Frame-ending delimiter. */
+	public static final byte[] FRAME_TAIL = { ControlCharacter.DLE, ControlCharacter.ETX };
 
+	/** Receive error message. */
+	private static final String ERROR_RECEIVE = "Error in reading from Newton device - connection stopped!";
+
+	/**
+	 * Constructs a new docking frame.
+	 */
+	public DockingFrame() {
+		super();
+	}
+
+	/**
+	 * Receive a frame. Ignores garbage data.
+	 * 
+	 * @param in
+	 *            the input.
+	 * @throws IOException
+	 *             if an I/O error occurs.
+	 */
+	public void receive(InputStream in) throws IOException {
+		int frameLength = FRAME_PREAMBLE.length;
+		int state = 0;
+		int b;
+		FrameCheckSequence fcs = new FrameCheckSequence();
+
+		while (state < frameLength) {
+			b = in.read();
+			if (b < 0) {
+				throw new IOException(ERROR_RECEIVE);
+			}
+			if (b == FRAME_PREAMBLE[state]) {
+				state++;
+			} else {
+				state = 0;
+			}
+		}
+	}
 }
---
+++ trunk/src/net/sf/jncu/protocol/FrameCheckSequence.java
@@ -0,0 +1,63 @@
+package net.sf.jncu.protocol;
+
+import java.util.zip.Checksum;
+
+/**
+ * Frame Check Sequence.
+ */
+public class FrameCheckSequence implements Checksum {
+
+	/** 16 bit value. */ 
+	private int fcsWord;
+
+	/** Creates a new FCS. */
+	public FrameCheckSequence() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.zip.Checksum#getValue()
+	 */
+	@Override
+	public long getValue() {
+		return fcsWord;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.zip.Checksum#reset()
+	 */
+	@Override
+	public void reset() {
+		fcsWord = 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.zip.Checksum#update(byte[], int, int)
+	 */
+	@Override
+	public void update(byte[] b, int off, int len) {
+		for (int i = off; i < len; i++) {
+			update(b[i]);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.zip.Checksum#update(int)
+	 */
+	@Override
+	public void update(int b) {
+		int pow = 1;
+		for (int i = 0; i < 8; i++) {
+			if ((((fcsWord % 256) & 0x01) == 0x01) ^ ((b & pow) == pow)) {
+				fcsWord = (fcsWord / 2) ^ 0xa001;
+			} else {
+				fcsWord /= 2;
+			}
+			pow *= 2;
+		}
+	}
+}
