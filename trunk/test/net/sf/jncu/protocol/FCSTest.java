f38c0de7d6a47f7a22305ae66970cba5e032e164
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-02-02 12:31:32 +0000

    test connection.

---
+++ trunk/test/net/sf/jncu/protocol/FCSTest.java
@@ -0,0 +1,17 @@
+package net.sf.jncu.protocol;
+
+import junit.framework.TestCase;
+
+public class FCSTest extends TestCase {
+
+	public FCSTest() {
+		super();
+	}
+
+	/**
+	 * Test the FCS of Newton's "attempt to connect."
+	 */
+	public void testConnect() {
+	}
+
+}
