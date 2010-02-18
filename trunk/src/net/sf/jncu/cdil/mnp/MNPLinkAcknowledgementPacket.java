fb14b487d29569613b89be9cdfb7bfe09e9133fc
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-02-18 14:32:28 +0000

    MNP packet implementations.

--- trunk/src/net/sf/jncu/cdil/mnp/MNPLinkAcknowledgementPacket.java
+++ trunk/src/net/sf/jncu/cdil/mnp/MNPLinkAcknowledgementPacket.java
@@ -9,12 +9,31 @@ public class MNPLinkAcknowledgementPacket extends MNPPacket {
 
 	private byte sequence;
 	private byte credit;
+	private byte[] data;
 
 	/**
-	 * Creates a new MNP Link Acknowledgement packet.
+	 * Creates a new MNP LA packet.
 	 */
 	public MNPLinkAcknowledgementPacket() {
 		super();
+	}
+
+	@Override
+	public int deserialize(byte[] payload) {
+		int offset = super.deserialize(payload);
+
+		sequence = payload[offset++];
+		credit = payload[offset++];
+		data = new byte[payload.length - offset];
+		System.arraycopy(payload, offset, data, 0, data.length);
+		offset += data.length;
+
+		return offset;
+	}
+
+	@Override
+	public byte[] serialize() {
+		return new byte[] { 3, LA, sequence, credit };
 	}
 
 }
--- trunk/src/net/sf/jncu/cdil/mnp/MNPLinkTransferPacket.java
+++ trunk/src/net/sf/jncu/cdil/mnp/MNPLinkTransferPacket.java
@@ -7,11 +7,36 @@ package net.sf.jncu.cdil.mnp;
  */
 public class MNPLinkTransferPacket extends MNPPacket {
 
+	private byte[] data;
+	private byte sequence;
+
 	/**
-	 * Creates a new MNP Link Transfer packet.
+	 * Creates a new MNP LT packet.
 	 */
 	public MNPLinkTransferPacket() {
 		super();
+	}
+
+	@Override
+	public int deserialize(byte[] payload) {
+		int offset = super.deserialize(payload);
+
+		sequence = payload[offset++];
+		data = new byte[payload.length - offset];
+		System.arraycopy(payload, offset, data, 0, data.length);
+		offset += data.length;
+
+		return offset;
+	}
+
+	@Override
+	public byte[] serialize() {
+		byte[] payload = new byte[3 + data.length];
+		payload[0] = 2;
+		payload[1] = LT;
+		payload[2] = sequence;
+		System.arraycopy(data, 0, payload, 3, data.length);
+		return payload;
 	}
 
 }
