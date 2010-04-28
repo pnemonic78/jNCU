929a6b418b246959b9b016a9961925bd46a19340
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-04-28 09:58:29 +0000

    naming. encoding data.

--- trunk/src/net/sf/jncu/protocol/DockCommandToNewton.java
+++ trunk/src/net/sf/jncu/protocol/DockCommandToNewton.java
@@ -36,34 +36,62 @@ public abstract class DockCommandToNewton extends DockCommand {
 	 * @return the payload.
 	 */
 	public byte[] getPayload() {
-		ByteArrayOutputStream frame = new ByteArrayOutputStream();
-		ByteArrayOutputStream buf = new ByteArrayOutputStream();
+		ByteArrayOutputStream payload = new ByteArrayOutputStream();
+		ByteArrayOutputStream dataStream = getData();
+		int length = 0;
+		byte[] data = null;
+
 		try {
-			byte[] data = encode();
-			int length = (data == null) ? 0 : data.length;
+			if (dataStream != null) {
+				dataStream.flush();
+				dataStream.close();
+				data = dataStream.toByteArray();
+				length = data.length;
+			}
 			setLength(length);
 
-			buf.write(kDNewtonDockBytes);
-			buf.write(cmdBytes);
-			buf.write((length >> 24) & 0xFF);
-			buf.write((length >> 16) & 0xFF);
-			buf.write((length >> 8) & 0xFF);
-			buf.write((length >> 0) & 0xFF);
-			buf.write(data);
-			buf.close();
-
-			// frame.write(buf.size());
-			frame.write(buf.toByteArray());
-			frame.close();
+			payload.write(kDNewtonDockBytes);
+			payload.write(commandBytes);
+			ntohl(length, payload);
+			if (data != null) {
+				payload.write(data);
+			}
+			// 4-byte align
+			switch (payload.size() & 3) {
+			case 1:
+				payload.write(0);
+			case 2:
+				payload.write(0);
+			case 3:
+				payload.write(0);
+				break;
+			}
+			payload.close();
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-		return frame.toByteArray();
+		return payload.toByteArray();
 	}
 
 	/**
 	 * Encode the data to write.
 	 */
-	protected abstract byte[] encode();
+	protected ByteArrayOutputStream getData() {
+		return null;
+	}
 
+	/**
+	 * Write 4 bytes as an unsigned integer in network byte order (Big Endian).
+	 * 
+	 * @param n
+	 *            the number.
+	 * @param frame
+	 *            the frame data.
+	 */
+	protected void ntohl(int n, ByteArrayOutputStream frame) {
+		frame.write((n >> 24) & 0xFF);
+		frame.write((n >> 16) & 0xFF);
+		frame.write((n >> 8) & 0xFF);
+		frame.write((n >> 0) & 0xFF);
+	}
 }
