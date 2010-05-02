085d734699416a3b052b120d92c5e2210d5577df
mwaisberg <pnemonic78@users.noreply.github.com>
2010-05-02 13:31:46 +0000

    implement.

--- src/net/sf/jncu/newton/stream/NSOFLargeBinary.java
+++ src/net/sf/jncu/newton/stream/NSOFLargeBinary.java
@@ -1,17 +1,61 @@
 package net.sf.jncu.newton.stream;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Newton Streamed Object Format - Large Binary.
+ * Newton Streamed Object Format - Large Binary Object.<br>
+ * Also known as Virtual Binary Object (VBO).
+ * <p>
+ * A compandor/compander (compressor-expander) is an object that transparently
+ * compresses data as it is stored and expands data as it is read.
  * 
  * @author Moshe
  */
 public class NSOFLargeBinary extends NSOFBinaryObject {
 
 	public static final NSOFSymbol NS_CLASS = new NSOFSymbol("largeBinary");
+
+	/**
+	 * Specifies the use of the Lempel-Ziv compressor-expander.
+	 */
+	public static final String COMPANDER_LZ = "TLZStoreCompander";
+	/**
+	 * Specifies the use of a compander specialized for pixel map data. (A
+	 * bitmap is a pixel map having a bit depth of 1.) This compander assumes
+	 * that the data in the VBO is a pixel map and that the pixel map data is
+	 * 32-bit aligned; that is, the length of the rows in the pixel map is an
+	 * even multiple of 4 bytes.
+	 */
+	public static final String COMPANDER_PIXELMAP = "TPixelMapCompander";
+
+	/** Data is compressed. */
+	protected static final byte FLAG_COMPRESSED = 1;
+	/** Data is not compressed. */
+	protected static final byte FLAG_UNCOMPRESSED = 0;
+
+	private boolean compressed = false;
+	/**
+	 * A string value specifying the implementation of the store compander
+	 * protocol used when the VBO created by this object is written to or read
+	 * from a soup entry. If the value of this parameter is nil, an uncompressed
+	 * object is created.
+	 * 
+	 * @see #COMPANDER_LZ
+	 * @see #COMPANDER_PIXELMAP
+	 */
+	private String companderName;
+	/**
+	 * Arguments for instantiating the specified compander. In the current
+	 * implementation, always pass nil as the value of this parameter.
+	 * <p>
+	 *"Because both companders provided by the current system initialize
+	 * themselves automatically, you must always pass nil as the value of the
+	 * companderArgs parameter."
+	 */
+	private byte[] companderArgs;
 
 	/**
 	 * Constructs a new large binary object.
@@ -32,21 +76,43 @@ public class NSOFLargeBinary extends NSOFBinaryObject {
 		NSOFSymbol symbol = (NSOFSymbol) decoder.decode(in);
 		setNSClass(symbol);
 		// compressed? (non-zero means compressed) (byte)
-		// TODO implement me!
+		int compressed = in.read();
+		if (compressed == -1) {
+			throw new EOFException();
+		}
+		setCompressed(compressed != FLAG_UNCOMPRESSED);
 		// Number of bytes of data (long)
-		// TODO implement me!
+		int numBytesData = htonl(in);
 		// Number of characters in compander name (long)
-		// TODO implement me!
-		// Number of byte of compander parameters (long)
-		// TODO implement me!
+		int numBytesCompanderName = htonl(in);
+		// Number of bytes of compander parameters (long)
+		int numBytesCompanderArgs = htonl(in);
 		// Reserved (encode zero, ignore when decoding) (long)
-		// TODO implement me!
+		htonl(in);
 		// Compander name (bytes)
-		// TODO implement me!
+		if (numBytesCompanderName == 0) {
+			setCompanderName(null);
+		} else {
+			byte[] companderName = new byte[numBytesCompanderName];
+			in.read(companderName);
+			setCompanderName(new String(companderName));
+		}
 		// Compander parameters (bytes)
-		// TODO implement me!
+		if (numBytesCompanderArgs == 0) {
+			setCompanderArguments(null);
+		} else {
+			byte[] companderArgs = new byte[numBytesCompanderArgs];
+			in.read(companderArgs);
+			setCompanderArguments(companderArgs);
+		}
 		// Data (bytes)
-		// TODO implement me!
+		if (numBytesData == 0) {
+			setValue(null);
+		} else {
+			byte[] data = new byte[numBytesData];
+			in.read(data);
+			setValue(data);
+		}
 	}
 
 	/*
@@ -57,24 +123,91 @@ public class NSOFLargeBinary extends NSOFBinaryObject {
 	@Override
 	public void encode(OutputStream out) throws IOException {
 		out.write(LARGE_BINARY);
+		String companderName = getCompanderName();
+		byte[] companderNameBytes = (companderName == null) ? null : companderName.getBytes();
+		byte[] args = getCompanderArguments();
+		byte[] data = getValue();
 		// Class (object)
 		getNSClass().encode(out);
 		// compressed? (non-zero means compressed) (byte)
-		// TODO implement me!
+		out.write(isCompressed() ? FLAG_UNCOMPRESSED : FLAG_UNCOMPRESSED);
 		// Number of bytes of data (long)
-		// TODO implement me!
+		ntohl((data == null) ? 0 : data.length, out);
 		// Number of characters in compander name (long)
-		// TODO implement me!
-		// Number of byte of compander parameters (long)
-		// TODO implement me!
+		ntohl((companderNameBytes == null) ? 0 : companderNameBytes.length, out);
+		// Number of bytes of compander parameters (long)
+		ntohl((args == null) ? 0 : args.length, out);
 		// Reserved (encode zero, ignore when decoding) (long)
-		// TODO implement me!
+		ntohl(0, out);
 		// Compander name (bytes)
-		// TODO implement me!
+		if (companderNameBytes != null) {
+			out.write(companderNameBytes);
+		}
 		// Compander parameters (bytes)
-		// TODO implement me!
+		if (args != null) {
+			out.write(args);
+		}
 		// Data (bytes)
-		// TODO implement me!
+		if (data != null) {
+			out.write(data);
+		}
+	}
+
+	/**
+	 * Is the data compressed?
+	 * 
+	 * @return compressed?
+	 */
+	public boolean isCompressed() {
+		return compressed;
+	}
+
+	/**
+	 * Set data compression.
+	 * 
+	 * @param compressed
+	 *            compress?
+	 */
+	public void setCompressed(boolean compressed) {
+		this.compressed = compressed;
+	}
+
+	/**
+	 * Get the compander name.
+	 * 
+	 * @return the compander name.
+	 */
+	public String getCompanderName() {
+		return companderName;
+	}
+
+	/**
+	 * Set the compander name.
+	 * 
+	 * @param companderName
+	 *            the compander name.
+	 */
+	public void setCompanderName(String companderName) {
+		this.companderName = companderName;
+	}
+
+	/**
+	 * Get the compander arguments/parameters.
+	 * 
+	 * @return the compander arguments.
+	 */
+	public byte[] getCompanderArguments() {
+		return companderArgs;
+	}
+
+	/**
+	 * Set the compander arguments/parameters.
+	 * 
+	 * @param companderArgs
+	 *            the compander arguments.
+	 */
+	public void setCompanderArguments(byte[] companderArgs) {
+		this.companderArgs = companderArgs;
 	}
 
 }
