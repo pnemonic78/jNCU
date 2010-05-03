49b6c46c2194b7add00d98f1bb722eaf5154b334
mwaisberg <pnemonic78@users.noreply.github.com>
2010-05-03 09:23:10 +0000

    new

---
+++ test/net/sf/jncu/newton/stream/NSOFExample.java
@@ -0,0 +1,30 @@
+package net.sf.jncu.newton.stream;
+
+import junit.framework.TestCase;
+
+/**
+ * @author moshew
+ */
+public class NSOFExample extends TestCase {
+
+	/**
+	 * Creates a new test case.
+	 */
+	public NSOFExample() {
+		super();
+	}
+
+	/**
+	 * Test encoding.
+	 */
+	public void testEncode() {
+		// TODO implement me!
+	}
+
+	/**
+	 * Test decoding.
+	 */
+	public void testDecode() {
+		// TODO implement me!
+	}
+}
