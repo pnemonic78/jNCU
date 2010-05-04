49e0f0cd62add54341a4d59080a971a2af704c3a
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-05-04 13:18:45 +0000

    new

---
+++ trunk/test/net/sf/jncu/cdil/mnp/DESTest.java
@@ -0,0 +1,22 @@
+package net.sf.jncu.cdil.mnp;
+
+import net.sf.junit.SFTestCase;
+
+/**
+ * DES test case.
+ * 
+ * @author moshew
+ */
+public class DESTest extends SFTestCase {
+
+	/**
+	 * Creates a new test case.
+	 */
+	public DESTest() {
+		super();
+	}
+
+	public void testNewtonKey() {
+		// TODO implement me!
+	}
+}
