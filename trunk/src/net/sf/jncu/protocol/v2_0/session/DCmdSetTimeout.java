76632f7e374da1e46aee041528edf5d1a55e68ce
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2010-05-04 09:00:11 +0000

    new commands.

---
+++ trunk/src/net/sf/jncu/protocol/v2_0/session/DCmdSetTimeout.java
@@ -0,0 +1,61 @@
+package net.sf.jncu.protocol.v2_0.session;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import net.sf.jncu.protocol.DockCommandToNewton;
+
+/**
+ * <tt>kDSetTimeout</tt><br>
+ * This command sets the timeout for the connection (the time the Newton will
+ * wait to receive data for it disconnects). This time is usually set to 30
+ * seconds.
+ * 
+ * <pre>
+ * 'stim' 
+ * length
+ * timeout in seconds
+ * </pre>
+ * 
+ * @author moshew
+ */
+public class DCmdSetTimeout extends DockCommandToNewton {
+
+	public static final String COMMAND = "stim";
+
+	private int timeout = 30000;
+
+	/**
+	 * Creates a new command.
+	 */
+	public DCmdSetTimeout() {
+		super(COMMAND);
+	}
+
+	/**
+	 * Get the timeout.
+	 * 
+	 * @return the timeout in seconds.
+	 */
+	public int getTimeout() {
+		return timeout;
+	}
+
+	/**
+	 * Set the timeout.
+	 * 
+	 * @param timeout
+	 *            the timeout in seconds.
+	 */
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+
+	@Override
+	protected ByteArrayOutputStream getCommandData() throws IOException {
+		ByteArrayOutputStream data = new ByteArrayOutputStream();
+		ntohl(getTimeout(), data);
+		return data;
+	}
+
+}
