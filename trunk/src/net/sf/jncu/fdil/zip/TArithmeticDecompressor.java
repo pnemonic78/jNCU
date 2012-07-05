4c0acab8736c1f4914d6f356b0b1a441a0d480b3
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2012-07-05 14:34:11 +0000

    new

---
+++ trunk/src/net/sf/jncu/fdil/zip/TArithmeticDecompressor.java
@@ -0,0 +1,43 @@
+/*
+ * Source file of the jNCU project.
+ * Copyright (c) 2010. All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * Contributors can be contacted by electronic mail via the project Web pages:
+ * 
+ * http://sourceforge.net/projects/jncu
+ * 
+ * http://jncu.sourceforge.net/
+ *
+ * Contributor(s):
+ *   Moshe Waisberg
+ * 
+ */
+package net.sf.jncu.fdil.zip;
+
+import java.io.InputStream;
+import java.util.zip.InflaterInputStream;
+
+/**
+ * @author mwaisberg
+ * 
+ */
+public class TArithmeticDecompressor extends TDecompressor {
+
+	/**
+	 * Creates a new decompressor.
+	 */
+	public TArithmeticDecompressor() {
+	}
+
+	@Override
+	protected InflaterInputStream createInflaterStream(InputStream in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
