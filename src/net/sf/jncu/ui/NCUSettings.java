263e1d8fcd6d6170097ad43ff97e8dabdfafd27c
mwaisberg <pnemonic78@users.noreply.github.com>
2012-07-13 13:20:29 +0000

    fix compiler warnings.

--- src/net/sf/jncu/ui/NCUSettings.java
+++ src/net/sf/jncu/ui/NCUSettings.java
@@ -64,8 +64,8 @@ public class NCUSettings extends JDialog {
 	private JPanel tabDock = null;
 	private JLabel labelPort = null;
 	private JLabel labelSpeed = null;
-	private JComboBox listPort = null;
-	private JComboBox listSpeed = null;
+	private JComboBox<String> listPort = null;
+	private JComboBox<Integer> listSpeed = null;
 	private JCheckBox checkListen = null;
 	private JLabel labelFolder = null;
 	private JLabel labelFolderPath = null;
@@ -189,12 +189,12 @@ public class NCUSettings extends JDialog {
 	public void setSettings(Settings settings) {
 		this.settings = settings;
 
-		JComboBox ports = getListPort();
+		JComboBox<String> ports = getListPort();
 		ports.removeAllItems();
 		for (String port : settings.getPorts()) {
 			ports.addItem(port);
 		}
-		JComboBox speed = getListSpeed();
+		JComboBox<Integer> speed = getListSpeed();
 		speed.setSelectedItem(settings.getPortSpeed());
 		getCheckListen().setSelected(settings.isListen());
 
@@ -353,9 +353,9 @@ public class NCUSettings extends JDialog {
 	 * 
 	 * @return javax.swing.JComboBox
 	 */
-	private JComboBox getListPort() {
+	private JComboBox<String> getListPort() {
 		if (listPort == null) {
-			listPort = new JComboBox();
+			listPort = new JComboBox<String>();
 		}
 		return listPort;
 	}
@@ -365,9 +365,9 @@ public class NCUSettings extends JDialog {
 	 * 
 	 * @return javax.swing.JComboBox
 	 */
-	private JComboBox getListSpeed() {
+	private JComboBox<Integer> getListSpeed() {
 		if (listSpeed == null) {
-			listSpeed = new JComboBox();
+			listSpeed = new JComboBox<Integer>();
 			listSpeed.addItem(MNPSerialPort.BAUD_2400);
 			listSpeed.addItem(MNPSerialPort.BAUD_4800);
 			listSpeed.addItem(MNPSerialPort.BAUD_9600);
