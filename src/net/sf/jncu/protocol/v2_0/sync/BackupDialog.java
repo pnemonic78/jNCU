48e91ceed8a53bfcf0cb33074067e9d873794a86
mwaisberg <pnemonic78@users.noreply.github.com>
2012-07-16 15:52:27 +0000

    backup dialog.

--- src/net/sf/jncu/protocol/v2_0/sync/BackupDialog.java
+++ src/net/sf/jncu/protocol/v2_0/sync/BackupDialog.java
@@ -23,22 +23,36 @@ import java.awt.BorderLayout;
 import java.awt.Dialog;
 import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-import javax.swing.AbstractListModel;
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 
+import net.sf.jncu.newton.os.Soup;
+import net.sf.jncu.newton.os.Store;
+import net.sf.jncu.protocol.v2_0.app.AppName;
+import net.sf.swing.CheckListCellRenderer;
 import net.sf.swing.SwingUtils;
 
 /**
@@ -53,7 +67,55 @@ public class BackupDialog extends JDialog {
 	}
 
 	private static final String TITLE = "Backup";
-	private JButton cancelButton;
+	private JButton buttonCancel;
+	private JButton buttonBackup;
+	private JList listStores;
+	private JList listInformation;
+	private JCheckBox checkPackages;
+	private SyncOptions options;
+	private final MouseListener listMouseClicked = new MouseAdapter() {
+		public void mouseClicked(MouseEvent event) {
+			JList list = (JList) event.getSource();
+
+			// Get index of item clicked
+			int index = list.locationToIndex(event.getPoint());
+			if (index < 0)
+				return;
+			JCheckBox item = (JCheckBox) list.getModel().getElementAt(index);
+			if (item == null)
+				return;
+
+			// Toggle selected state
+			item.setSelected(!item.isSelected());
+
+			// Repaint cell
+			list.repaint(list.getCellBounds(index, index));
+		}
+	};
+	private final KeyListener listkeyClicked = new KeyAdapter() {
+		@Override
+		public void keyReleased(KeyEvent event) {
+			int key = event.getKeyCode();
+			if ((key != KeyEvent.VK_SPACE) && (key != KeyEvent.VK_ENTER))
+				return;
+
+			JList list = (JList) event.getSource();
+
+			// Get index of item clicked
+			int index = list.getSelectedIndex();
+			if (index < 0)
+				return;
+			JCheckBox item = (JCheckBox) list.getModel().getElementAt(index);
+			if (item == null)
+				return;
+
+			// Toggle selected state
+			item.setSelected(!item.isSelected());
+
+			// Repaint cell
+			list.repaint(list.getCellBounds(index, index));
+		}
+	};
 
 	/**
 	 * Creates a new dialog.
@@ -113,30 +175,18 @@ public class BackupDialog extends JDialog {
 
 		JPanel panelStores = new JPanel();
 		panelStores.setOpaque(false);
-		GridBagConstraints gbc_panelStores = new GridBagConstraints();
-		gbc_panelStores.insets = new Insets(0, 0, 5, 5);
-		gbc_panelStores.fill = GridBagConstraints.BOTH;
-		gbc_panelStores.gridx = 0;
-		gbc_panelStores.gridy = 4;
 		panelContents.add(panelStores, BorderLayout.WEST);
 		panelStores.setLayout(new BorderLayout(5, 5));
 		panelStores.setBorder(BorderFactory.createTitledBorder("Backup From:"));
 
-		JList listStores = new JList();
+		listStores = new JList();
+		listStores.setCellRenderer(new CheckListCellRenderer());
 		listStores.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		panelStores.add(listStores, BorderLayout.CENTER);
 		listStores.setVisibleRowCount(5);
-		listStores.setModel(new AbstractListModel() {
-			String[] values = new String[] { "Internal" };
-
-			public int getSize() {
-				return values.length;
-			}
-
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
+		listStores.addMouseListener(listMouseClicked);
+		listStores.addKeyListener(listkeyClicked);
+		JScrollPane scrollStores = new JScrollPane(listStores);
+		panelStores.add(scrollStores, BorderLayout.CENTER);
 
 		JPanel panelStoresButtons = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panelStoresButtons.getLayout();
@@ -144,57 +194,73 @@ public class BackupDialog extends JDialog {
 		panelStores.add(panelStoresButtons, BorderLayout.SOUTH);
 
 		JButton btnStoresSelectAll = new JButton("Select All");
+		btnStoresSelectAll.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectAll(listStores);
+			}
+		});
 		panelStoresButtons.add(btnStoresSelectAll);
 
 		JButton btnStoresClearAll = new JButton("Clear All");
+		btnStoresClearAll.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearAll(listStores);
+			}
+		});
 		panelStoresButtons.add(btnStoresClearAll);
 
 		JPanel panelInfo = new JPanel();
 		panelInfo.setOpaque(false);
-		GridBagConstraints gbc_panelSoups = new GridBagConstraints();
-		gbc_panelSoups.insets = new Insets(0, 0, 5, 0);
-		gbc_panelSoups.fill = GridBagConstraints.BOTH;
-		gbc_panelSoups.gridx = 1;
-		gbc_panelSoups.gridy = 4;
-		panelContents.add(panelInfo, BorderLayout.EAST);
+		panelContents.add(panelInfo, BorderLayout.CENTER);
 		panelInfo.setLayout(new BorderLayout(5, 5));
 		panelInfo.setBorder(BorderFactory.createTitledBorder("Information:"));
-
-		JPanel panelInfoButtons = new JPanel();
-		panelInfoButtons.setOpaque(false);
-		FlowLayout flowLayout_1 = (FlowLayout) panelInfoButtons.getLayout();
-		flowLayout_1.setAlignment(FlowLayout.TRAILING);
-		panelInfo.add(panelInfoButtons, BorderLayout.SOUTH);
-
-		JButton btnInfoSelectAll = new JButton("Select All");
-		panelInfoButtons.add(btnInfoSelectAll);
-
-		JButton btnInfoClearAll = new JButton("Clear All");
-		panelInfoButtons.add(btnInfoClearAll);
 
 		JPanel panelInfoCenter = new JPanel();
 		panelInfoCenter.setOpaque(false);
 		panelInfo.add(panelInfoCenter, BorderLayout.CENTER);
 		panelInfoCenter.setLayout(new BorderLayout(0, 0));
 
-		JList listInformation = new JList();
+		listInformation = new JList();
 		listInformation.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		panelInfoCenter.add(listInformation, BorderLayout.CENTER);
-		listInformation.setModel(new AbstractListModel() {
-			String[] values = new String[] { "Calls", "SBM", "System information", "TermLimit", "Time Zones", "Other information" };
+		listInformation.setVisibleRowCount(5);
+		listInformation.setCellRenderer(new CheckListCellRenderer());
+		listInformation.addMouseListener(listMouseClicked);
+		listInformation.addKeyListener(listkeyClicked);
+		JScrollPane scrollInfo = new JScrollPane(listInformation);
+		panelInfoCenter.add(scrollInfo, BorderLayout.CENTER);
 
-			public int getSize() {
-				return values.length;
-			}
+		JPanel panelInfoButtons = new JPanel();
+		panelInfoCenter.add(panelInfoButtons, BorderLayout.SOUTH);
+		panelInfoButtons.setOpaque(false);
+		FlowLayout flowLayout_1 = (FlowLayout) panelInfoButtons.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.TRAILING);
 
-			public Object getElementAt(int index) {
-				return values[index];
+		JButton btnInfoSelectAll = new JButton("Select All");
+		btnInfoSelectAll.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectAll(listInformation);
 			}
 		});
-		listInformation.setVisibleRowCount(5);
+		panelInfoButtons.add(btnInfoSelectAll);
 
-		JCheckBox checkPackages = new JCheckBox("Include All Packages");
-		panelInfoCenter.add(checkPackages, BorderLayout.SOUTH);
+		JButton btnInfoClearAll = new JButton("Clear All");
+		btnInfoClearAll.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearAll(listInformation);
+			}
+		});
+		panelInfoButtons.add(btnInfoClearAll);
+
+		checkPackages = new JCheckBox("Include All Packages");
+		panelInfo.add(checkPackages, BorderLayout.SOUTH);
 		checkPackages.setSelected(true);
 
 		JPanel panelButtons = new JPanel();
@@ -202,37 +268,78 @@ public class BackupDialog extends JDialog {
 		panelContents.add(panelButtons, BorderLayout.SOUTH);
 		panelButtons.setLayout(new FlowLayout(FlowLayout.TRAILING, 5, 5));
 
-		JButton btnBackup = new JButton("Backup");
-		panelButtons.add(btnBackup);
+		buttonBackup = new JButton("Backup");
+		buttonBackup.addActionListener(new ActionListener() {
 
-		panelButtons.add(getCancelButton());
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				close();
+				// TODO Start backup...
+			}
+		});
+		panelButtons.add(buttonBackup);
 
-		pack();
+		buttonCancel = new JButton(Toolkit.getProperty("AWT.cancel", "Cancel"));
+		buttonCancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				close();
+			}
+		});
+		panelButtons.add(buttonCancel);
+
+		setSize(380, 300);
 		SwingUtils.centreInOwner(this);
 	}
 
 	public static void main(String[] args) {
+		SyncOptions options = new SyncOptions();
+		options.setPackages(true);
+
+		List<Store> stores = new ArrayList<Store>();
+		stores.add(new Store("Internal"));
+		stores.add(new Store("Store #2"));
+		stores.add(new Store("Store #3"));
+		stores.add(new Store("Store #4"));
+		stores.add(new Store("Store #5"));
+		stores.add(new Store("Store #6"));
+		stores.add(new Store("Store #7"));
+		stores.add(new Store("Store #8"));
+		stores.add(new Store("Store #9"));
+		stores.add(new Store("Store #10"));
+		stores.add(new Store("Store #11"));
+		stores.add(new Store("Store #12"));
+		options.setStores(stores);
+
+		List<Soup> soups = new ArrayList<Soup>();
+		soups.add(new Soup("Soup #1"));
+		soups.add(new Soup("Soup #3"));
+		soups.add(new Soup("Soup #5"));
+		soups.add(new Soup("Soup #7"));
+		soups.add(new Soup("Soup #9"));
+		soups.add(new Soup("Soup #11"));
+		stores.get(0).setSoups(soups);
+
+		soups = new ArrayList<Soup>();
+		soups.add(new Soup("Soup #2"));
+		soups.add(new Soup("Soup #4"));
+		soups.add(new Soup("Soup #6"));
+		soups.add(new Soup("Soup #8"));
+		soups.add(new Soup("Soup #10"));
+		soups.add(new Soup("Soup #12"));
+		stores.get(1).setSoups(soups);
+
+		soups = new ArrayList<Soup>();
+		soups.add(new Soup(AppName.NAME_BACKUP));
+		soups.add(new Soup(AppName.NAME_OTHER));
+		soups.add(new Soup(AppName.NAME_PACKAGES));
+		soups.add(new Soup(AppName.NAME_SYSTEM));
+		stores.get(2).setSoups(soups);
+
 		BackupDialog dialog = new BackupDialog();
+		dialog.setSyncOptions(options);
 		dialog.setVisible(true);
-	}
-
-	/**
-	 * Get the "cancel" button. Closes the window.
-	 * 
-	 * @return the button.
-	 */
-	private JButton getCancelButton() {
-		if (cancelButton == null) {
-			cancelButton = new JButton(Toolkit.getProperty("AWT.cancel", "Cancel"));
-			cancelButton.addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent event) {
-					close();
-				}
-			});
-		}
-		return cancelButton;
 	}
 
 	/**
@@ -243,6 +350,111 @@ public class BackupDialog extends JDialog {
 			setVisible(false);
 			SwingUtils.postWindowClosing(this);
 		}
+	}
+
+	/**
+	 * Get the list of stores.
+	 * 
+	 * @return the list.
+	 */
+	private JList getListStores() {
+		return listStores;
+	}
+
+	/**
+	 * Get the list of information soups.
+	 * 
+	 * @return the list.
+	 */
+	private JList getListInformation() {
+		return listInformation;
+	}
+
+	/**
+	 * Get the checkbox to include all packages.
+	 * 
+	 * @return the checkbox
+	 */
+	private JCheckBox getCheckPackages() {
+		return checkPackages;
+	}
+
+	/**
+	 * Get the sync options.
+	 * 
+	 * @return the options.
+	 */
+	public SyncOptions getSyncOptions() {
+		return options;
+	}
+
+	/**
+	 * Set the sync options.
+	 * 
+	 * @param options
+	 *            the options.
+	 */
+	public void setSyncOptions(SyncOptions options) {
+		this.options = options;
+
+		DefaultListModel modelStores = new DefaultListModel();
+		DefaultListModel modelSoups = new DefaultListModel();
+		SortedSet<Soup> soups = new TreeSet<Soup>();
+
+		for (Store store : options.getStores()) {
+			modelStores.addElement(new JCheckBox(store.getName(), true));
+			soups.addAll(store.getSoups());
+		}
+		String soupName;
+		for (Soup soup : soups) {
+			soupName = soup.getName();
+			if (AppName.NAME_BACKUP.equals(soupName))
+				continue;
+			if (AppName.NAME_PACKAGES.equals(soupName))
+				continue;
+			modelSoups.addElement(new JCheckBox(soupName, true));
+		}
+		getListStores().setModel(modelStores);
+		getListInformation().setModel(modelSoups);
+
+		getCheckPackages().setSelected(options.isPackages());
+
+		pack();
+		SwingUtils.centreInOwner(this);
+	}
+
+	/**
+	 * Check all items.
+	 * 
+	 * @param list
+	 *            the list.
+	 */
+	private void selectAll(JList list) {
+		ListModel model = list.getModel();
+		int size = model.getSize();
+		JCheckBox check;
+		for (int i = 0; i < size; i++) {
+			check = (JCheckBox) model.getElementAt(i);
+			check.setSelected(true);
+		}
+		list.repaint();
+	}
+
+	/**
+	 * Un-check all items.
+	 * 
+	 * @param list
+	 *            the list.
+	 */
+	private void clearAll(JList list) {
+		ListModel model = list.getModel();
+		int size = model.getSize();
+		JCheckBox check;
+		for (int i = 0; i < size; i++) {
+			check = (JCheckBox) model.getElementAt(i);
+			check.setSelected(false);
+		}
+		list.repaint();
 	}
 
 }
