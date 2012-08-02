1750399b1b6816ea2206269e772f4149788f0c1a
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2012-08-02 14:53:12 +0000

    array equals.

--- trunk/test/net/sf/jncu/fdil/NSOFEncoding.java
+++ trunk/test/net/sf/jncu/fdil/NSOFEncoding.java
@@ -76,7 +76,7 @@ public class NSOFEncoding extends SFTestCase {
 	 * @throws Exception
 	 */
 	@Test
-	public void testStrings() throws Exception {
+	public void testStringRepeatNotes() throws Exception {
 		NSOFSymbol nohint = new NSOFSymbol("string.nohint");
 		String val = "Repeat Notes";
 
@@ -95,4 +95,235 @@ public class NSOFEncoding extends SFTestCase {
 		encoder.flatten(str, out);
 		assertEquals(REPEAT_NOTES, out.toByteArray());
 	}
+
+	// extracted from DSoupInfo command.
+	private static final byte[] SOUP_INFO_NAMES_EXTERNAL = { 2, 6, 2, 7, 7, 115, 111, 117, 112, 68, 101, 102, 7, 17, 78, 67, 75, 76, 97, 115, 116, 66, 97, 99, 107, 117, 112, 84,
+			105, 109, 101, 6, 6, 7, 8, 111, 119, 110, 101, 114, 65, 112, 112, 7, 9, 117, 115, 101, 114, 68, 101, 115, 99, 114, 7, 7, 105, 110, 100, 101, 120, 101, 115, 7, 4, 110,
+			97, 109, 101, 7, 8, 117, 115, 101, 114, 78, 97, 109, 101, 7, 12, 111, 119, 110, 101, 114, 65, 112, 112, 78, 97, 109, 101, 7, 8, 99, 97, 114, 100, 102, 105, 108, 101,
+			8, 60, 0, 83, 0, 111, 0, 117, 0, 112, 0, 32, 0, 119, 0, 105, 0, 116, 0, 104, 0, 32, 0, 78, 0, 97, 0, 109, 0, 101, 0, 115, 0, 32, 0, 97, 0, 110, 0, 100, 0, 32, 0, 65,
+			0, 100, 0, 100, 0, 114, 0, 101, 0, 115, 0, 115, 0, 101, 0, 115, 0, 0, 5, 2, 6, 3, 7, 4, 116, 121, 112, 101, 7, 4, 112, 97, 116, 104, 7, 9, 115, 116, 114, 117, 99, 116,
+			117, 114, 101, 7, 6, 115, 116, 114, 105, 110, 103, 7, 6, 115, 111, 114, 116, 111, 110, 7, 4, 115, 108, 111, 116, 6, 4, 9, 14, 9, 15, 7, 4, 116, 97, 103, 115, 9, 16, 9,
+			21, 7, 6, 108, 97, 98, 101, 108, 115, 10, 9, 19, 8, 12, 0, 78, 0, 97, 0, 109, 0, 101, 0, 115, 0, 0, 9, 23, 9, 23, 0, -1, 13, -99, -118, 60 };
+
+	/**
+	 * Test soup encoding of Name on External card.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSoupInfoNamesExternal() throws Exception {
+		assertEquals(265, SOUP_INFO_NAMES_EXTERNAL.length);
+
+		InputStream in = new ByteArrayInputStream(SOUP_INFO_NAMES_EXTERNAL);
+		NSOFDecoder decoder = new NSOFDecoder();
+		NSOFObject o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		NSOFFrame f = (NSOFFrame) o;
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream(SOUP_INFO_NAMES_EXTERNAL.length);
+		NSOFEncoder encoder = new NSOFEncoder();
+		encoder.flatten(f, out);
+		assertEquals(SOUP_INFO_NAMES_EXTERNAL, out.toByteArray());
+	}
+
+	// extracted from DSoupInfo command.
+	private static final byte[] SOUP_INFO_NAMES_INTERNAL = { 2, 6, 3, 7, 7, 115, 111, 117, 112, 68, 101, 102, 7, 17, 78, 67, 75, 76, 97, 115, 116, 66, 97, 99, 107, 117, 112, 84,
+			105, 109, 101, 7, 12, 99, 117, 115, 116, 111, 109, 70, 105, 101, 108, 100, 115, 6, 6, 7, 8, 111, 119, 110, 101, 114, 65, 112, 112, 7, 9, 117, 115, 101, 114, 68, 101,
+			115, 99, 114, 7, 7, 105, 110, 100, 101, 120, 101, 115, 7, 4, 110, 97, 109, 101, 7, 8, 117, 115, 101, 114, 78, 97, 109, 101, 7, 12, 111, 119, 110, 101, 114, 65, 112,
+			112, 78, 97, 109, 101, 7, 8, 99, 97, 114, 100, 102, 105, 108, 101, 8, 60, 0, 83, 0, 111, 0, 117, 0, 112, 0, 32, 0, 119, 0, 105, 0, 116, 0, 104, 0, 32, 0, 78, 0, 97, 0,
+			109, 0, 101, 0, 115, 0, 32, 0, 97, 0, 110, 0, 100, 0, 32, 0, 65, 0, 100, 0, 100, 0, 114, 0, 101, 0, 115, 0, 115, 0, 101, 0, 115, 0, 0, 5, 2, 6, 3, 7, 4, 116, 121, 112,
+			101, 7, 4, 112, 97, 116, 104, 7, 9, 115, 116, 114, 117, 99, 116, 117, 114, 101, 7, 6, 115, 116, 114, 105, 110, 103, 7, 6, 115, 111, 114, 116, 111, 110, 7, 4, 115, 108,
+			111, 116, 6, 4, 9, 15, 9, 16, 7, 4, 116, 97, 103, 115, 9, 17, 9, 22, 7, 6, 108, 97, 98, 101, 108, 115, 10, 9, 20, 8, 12, 0, 78, 0, 97, 0, 109, 0, 101, 0, 115, 0, 0, 9,
+			24, 9, 24, 0, -1, 13, -99, -118, 60, 6, 8, 7, 7, 99, 117, 115, 116, 111, 109, 51, 7, 7, 99, 117, 115, 116, 111, 109, 56, 7, 7, 99, 117, 115, 116, 111, 109, 53, 7, 7,
+			99, 117, 115, 116, 111, 109, 50, 7, 7, 99, 117, 115, 116, 111, 109, 55, 7, 7, 99, 117, 115, 116, 111, 109, 52, 7, 7, 99, 117, 115, 116, 111, 109, 49, 7, 7, 99, 117,
+			115, 116, 111, 109, 54, 6, 1, 7, 5, 108, 97, 98, 101, 108, 8, 36, 0, 67, 0, 108, 0, 117, 0, 98, 0, 115, 0, 32, 0, 38, 0, 32, 0, 83, 0, 111, 0, 99, 0, 105, 0, 101, 0,
+			116, 0, 105, 0, 101, 0, 115, 0, 0, 6, 1, 9, 35, 8, 12, 0, 66, 0, 108, 0, 111, 0, 111, 0, 100, 0, 0, 6, 1, 9, 35, 8, 22, 0, 79, 0, 99, 0, 99, 0, 117, 0, 112, 0, 97, 0,
+			116, 0, 105, 0, 111, 0, 110, 0, 0, 6, 1, 9, 35, 8, 8, 0, 85, 0, 82, 0, 76, 0, 0, 6, 1, 9, 35, 8, 2, 0, 0, 6, 1, 9, 35, 8, 20, 0, 67, 0, 97, 0, 114, 0, 32, 0, 80, 0,
+			108, 0, 97, 0, 116, 0, 101, 0, 0, 6, 1, 9, 35, 8, 18, 0, 69, 0, 109, 0, 112, 0, 108, 0, 111, 0, 121, 0, 101, 0, 101, 0, 0, 6, 1, 9, 35, 8, 6, 0, 73, 0, 68, 0, 0 };
+
+	/**
+	 * Test soup encoding of Name on Internal card.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSoupInfoNamesInternal() throws Exception {
+		assertEquals(530, SOUP_INFO_NAMES_INTERNAL.length);
+
+		InputStream in = new ByteArrayInputStream(SOUP_INFO_NAMES_INTERNAL);
+		NSOFDecoder decoder = new NSOFDecoder();
+		NSOFObject o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		NSOFFrame f = (NSOFFrame) o;
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream(SOUP_INFO_NAMES_INTERNAL.length);
+		NSOFEncoder encoder = new NSOFEncoder();
+		encoder.flatten(f, out);
+		assertEquals(SOUP_INFO_NAMES_INTERNAL, out.toByteArray());
+	}
+
+	/**
+	 * Test soup encoding of Name on Internal card.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSoupInfoNamesInternalPartial() throws Exception {
+		assertEquals(530, SOUP_INFO_NAMES_INTERNAL.length);
+
+		InputStream in = new ByteArrayInputStream(SOUP_INFO_NAMES_INTERNAL);
+		NSOFDecoder decoder = new NSOFDecoder();
+		NSOFObject o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		NSOFFrame f = (NSOFFrame) o;
+
+		NSOFFrame f1 = new NSOFFrame();
+		f1.put("soupDef", f.get("soupDef"));
+		assertEquals(1, f1.size());
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		NSOFEncoder encoder = new NSOFEncoder();
+		encoder.flatten(f1, out);
+		in = new ByteArrayInputStream(out.toByteArray());
+		decoder = new NSOFDecoder();
+		o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		assertEquals(f1, o);
+
+		NSOFFrame f2 = new NSOFFrame();
+		f2.put("soupDef", f.get("soupDef"));
+		f2.put("NCKLastBackupTime", f.get("NCKLastBackupTime"));
+		assertEquals(2, f2.size());
+		out = new ByteArrayOutputStream();
+		encoder = new NSOFEncoder();
+		encoder.flatten(f2, out);
+		in = new ByteArrayInputStream(out.toByteArray());
+		decoder = new NSOFDecoder();
+		o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		assertEquals(f2, o);
+
+		NSOFFrame f3 = new NSOFFrame();
+		f3.put("NCKLastBackupTime", f.get("NCKLastBackupTime"));
+		f3.put("customFields", f.get("customFields"));
+		assertEquals(2, f3.size());
+		out = new ByteArrayOutputStream();
+		encoder = new NSOFEncoder();
+		encoder.flatten(f3, out);
+		in = new ByteArrayInputStream(out.toByteArray());
+		decoder = new NSOFDecoder();
+		o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		assertEquals(f3, o);
+	}
+
+	// {soupDef={ownerApp='cardfile, userDescr="Soup with Names and Addresses",
+	// indexes=[2 Elements], name="Names", userName="Names",
+	// ownerAppName="Names"}, NCKLastBackupTime=57107087, name="Names",
+	// signature=159604293}
+	private static final byte[] SOUP_NAMES_EXTERNAL = { 2, 6, 4, 7, 7, 115, 111, 117, 112, 68, 101, 102, 7, 17, 78, 67, 75, 76, 97, 115, 116, 66, 97, 99, 107, 117, 112, 84, 105,
+			109, 101, 7, 4, 110, 97, 109, 101, 7, 9, 115, 105, 103, 110, 97, 116, 117, 114, 101, 6, 6, 7, 8, 111, 119, 110, 101, 114, 65, 112, 112, 7, 9, 117, 115, 101, 114, 68,
+			101, 115, 99, 114, 7, 7, 105, 110, 100, 101, 120, 101, 115, 9, 3, 7, 8, 117, 115, 101, 114, 78, 97, 109, 101, 7, 12, 111, 119, 110, 101, 114, 65, 112, 112, 78, 97,
+			109, 101, 7, 8, 99, 97, 114, 100, 102, 105, 108, 101, 8, 60, 0, 83, 0, 111, 0, 117, 0, 112, 0, 32, 0, 119, 0, 105, 0, 116, 0, 104, 0, 32, 0, 78, 0, 97, 0, 109, 0, 101,
+			0, 115, 0, 32, 0, 97, 0, 110, 0, 100, 0, 32, 0, 65, 0, 100, 0, 100, 0, 114, 0, 101, 0, 115, 0, 115, 0, 101, 0, 115, 0, 0, 5, 2, 6, 3, 7, 4, 116, 121, 112, 101, 7, 4,
+			112, 97, 116, 104, 7, 9, 115, 116, 114, 117, 99, 116, 117, 114, 101, 7, 6, 115, 116, 114, 105, 110, 103, 7, 6, 115, 111, 114, 116, 111, 110, 7, 4, 115, 108, 111, 116,
+			6, 4, 9, 15, 9, 16, 7, 4, 116, 97, 103, 115, 9, 17, 9, 22, 7, 6, 108, 97, 98, 101, 108, 115, 10, 9, 20, 8, 12, 0, 78, 0, 97, 0, 109, 0, 101, 0, 115, 0, 0, 9, 24, 9,
+			24, 0, -1, 13, -99, -118, 60, 9, 24, 0, -1, 38, 13, 121, 20 };
+
+	/**
+	 * Test soup encoding of Name on External card.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSoupNamesExternal() throws Exception {
+		assertEquals(286, SOUP_NAMES_EXTERNAL.length);
+
+		InputStream in = new ByteArrayInputStream(SOUP_NAMES_EXTERNAL);
+		NSOFDecoder decoder = new NSOFDecoder();
+		NSOFObject o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		NSOFFrame f = (NSOFFrame) o;
+		NSOFObject n = f.get("name");
+		assertNotNull(n);
+		assertFalse(NSOFImmediate.isNil(n));
+		assertTrue(n instanceof NSOFString);
+		NSOFString name = (NSOFString) n;
+		assertEquals("Names", name.getValue());
+		NSOFObject s = f.get("signature");
+		assertNotNull(s);
+		assertFalse(NSOFImmediate.isNil(s));
+		assertTrue(s instanceof NSOFInteger);
+		NSOFInteger sig = (NSOFInteger) s;
+		assertEquals(159604293, sig.getValue());
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream(SOUP_NAMES_EXTERNAL.length);
+		NSOFEncoder encoder = new NSOFEncoder();
+		encoder.flatten(f, out);
+		assertEquals(SOUP_NAMES_EXTERNAL, out.toByteArray());
+	}
+
+	// {soupDef={ownerApp='cardfile, userDescr="Soup with Names and Addresses",
+	// indexes=[2 Elements], name="Names", userName="Names",
+	// ownerAppName="Names"}, NCKLastBackupTime=57107087,
+	// customFields={custom3={label="Clubs & Societies"},
+	// custom8={label="Blood"}, custom5={label="Occupation"},
+	// custom2={label="URL"}, custom7={label=""}, custom4={label="Car Plate"},
+	// custom1={label="Employee"}, custom6={label="ID"}}, name="Names",
+	// signature=123338611}
+	private static final byte[] SOUP_NAMES_INTERNAL = { 2, 6, 5, 7, 7, 115, 111, 117, 112, 68, 101, 102, 7, 17, 78, 67, 75, 76, 97, 115, 116, 66, 97, 99, 107, 117, 112, 84, 105,
+			109, 101, 7, 12, 99, 117, 115, 116, 111, 109, 70, 105, 101, 108, 100, 115, 7, 4, 110, 97, 109, 101, 7, 9, 115, 105, 103, 110, 97, 116, 117, 114, 101, 6, 6, 7, 8, 111,
+			119, 110, 101, 114, 65, 112, 112, 7, 9, 117, 115, 101, 114, 68, 101, 115, 99, 114, 7, 7, 105, 110, 100, 101, 120, 101, 115, 9, 4, 7, 8, 117, 115, 101, 114, 78, 97,
+			109, 101, 7, 12, 111, 119, 110, 101, 114, 65, 112, 112, 78, 97, 109, 101, 7, 8, 99, 97, 114, 100, 102, 105, 108, 101, 8, 60, 0, 83, 0, 111, 0, 117, 0, 112, 0, 32, 0,
+			119, 0, 105, 0, 116, 0, 104, 0, 32, 0, 78, 0, 97, 0, 109, 0, 101, 0, 115, 0, 32, 0, 97, 0, 110, 0, 100, 0, 32, 0, 65, 0, 100, 0, 100, 0, 114, 0, 101, 0, 115, 0, 115,
+			0, 101, 0, 115, 0, 0, 5, 2, 6, 3, 7, 4, 116, 121, 112, 101, 7, 4, 112, 97, 116, 104, 7, 9, 115, 116, 114, 117, 99, 116, 117, 114, 101, 7, 6, 115, 116, 114, 105, 110,
+			103, 7, 6, 115, 111, 114, 116, 111, 110, 7, 4, 115, 108, 111, 116, 6, 4, 9, 16, 9, 17, 7, 4, 116, 97, 103, 115, 9, 18, 9, 23, 7, 6, 108, 97, 98, 101, 108, 115, 10, 9,
+			21, 8, 12, 0, 78, 0, 97, 0, 109, 0, 101, 0, 115, 0, 0, 9, 25, 9, 25, 0, -1, 13, -99, -118, 60, 6, 8, 7, 7, 99, 117, 115, 116, 111, 109, 51, 7, 7, 99, 117, 115, 116,
+			111, 109, 56, 7, 7, 99, 117, 115, 116, 111, 109, 53, 7, 7, 99, 117, 115, 116, 111, 109, 50, 7, 7, 99, 117, 115, 116, 111, 109, 55, 7, 7, 99, 117, 115, 116, 111, 109,
+			52, 7, 7, 99, 117, 115, 116, 111, 109, 49, 7, 7, 99, 117, 115, 116, 111, 109, 54, 6, 1, 7, 5, 108, 97, 98, 101, 108, 8, 36, 0, 67, 0, 108, 0, 117, 0, 98, 0, 115, 0,
+			32, 0, 38, 0, 32, 0, 83, 0, 111, 0, 99, 0, 105, 0, 101, 0, 116, 0, 105, 0, 101, 0, 115, 0, 0, 6, 1, 9, 36, 8, 12, 0, 66, 0, 108, 0, 111, 0, 111, 0, 100, 0, 0, 6, 1, 9,
+			36, 8, 22, 0, 79, 0, 99, 0, 99, 0, 117, 0, 112, 0, 97, 0, 116, 0, 105, 0, 111, 0, 110, 0, 0, 6, 1, 9, 36, 8, 8, 0, 85, 0, 82, 0, 76, 0, 0, 6, 1, 9, 36, 8, 0, 0, 0, 6,
+			1, 9, 36, 8, 20, 0, 67, 0, 97, 0, 114, 0, 32, 0, 80, 0, 108, 0, 97, 0, 116, 0, 101, 0, 0, 6, 1, 9, 36, 8, 18, 0, 69, 0, 109, 0, 112, 0, 108, 0, 111, 0, 121, 0, 101, 0,
+			101, 0, 0, 6, 1, 9, 36, 8, 6, 0, 73, 0, 68, 0, 0, 9, 25, 0, -1, 29, 103, -3, -52 };
+
+	/**
+	 * Test soup encoding of Name on Internal card.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSoupNamesInternal() throws Exception {
+		assertEquals(551, SOUP_NAMES_INTERNAL.length);
+
+		InputStream in = new ByteArrayInputStream(SOUP_NAMES_INTERNAL);
+		NSOFDecoder decoder = new NSOFDecoder();
+		NSOFObject o = decoder.inflate(in);
+		assertNotNull(o);
+		assertTrue(o instanceof NSOFFrame);
+		NSOFFrame f = (NSOFFrame) o;
+		NSOFObject n = f.get("name");
+		assertNotNull(n);
+		assertFalse(NSOFImmediate.isNil(n));
+		assertTrue(n instanceof NSOFString);
+		NSOFString name = (NSOFString) n;
+		assertEquals("Names", name.getValue());
+		NSOFObject s = f.get("signature");
+		assertNotNull(s);
+		assertFalse(NSOFImmediate.isNil(s));
+		assertTrue(s instanceof NSOFInteger);
+		NSOFInteger sig = (NSOFInteger) s;
+		assertEquals(123338611, sig.getValue());
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream(SOUP_NAMES_INTERNAL.length);
+		NSOFEncoder encoder = new NSOFEncoder();
+		encoder.flatten(f, out);
+		assertEquals(SOUP_NAMES_INTERNAL, out.toByteArray());
+	}
+
 }
