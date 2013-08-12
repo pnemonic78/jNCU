ea0469d244f163f995efc8b96fb328e01d80f8cb
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2013-08-12 19:40:40 +0000

    license.

--- trunk/test/net/sf/jncu/protocol/app/DPackageTester.java
+++ trunk/test/net/sf/jncu/protocol/app/DPackageTester.java
@@ -1,3 +1,22 @@
+/*
+ * Source file of the jNCU project.
+ * Copyright (c) 2010. All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * Contributors can be contacted by electronic mail via the project Web pages:
+ * 
+ * http://sourceforge.net/projects/jncu
+ * 
+ * http://jncu.sourceforge.net/
+ *
+ * Contributor(s):
+ *   Moshe Waisberg
+ * 
+ */
 package net.sf.jncu.protocol.app;
 
 import java.io.File;
