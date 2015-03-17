12920a57b494cd731421968a5195503a94e27fa0
mwaisberg <mwaisberg@a1893dee-8e03-480e-a464-ad54b6681ee4>
2015-03-17 21:20:11 +0000

    dump frame.

---
+++ trunk/test/net/sf/jncu/fdil/FrameDump.java
@@ -0,0 +1,78 @@
+package net.sf.jncu.fdil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Dump a NSOF frame.
+ * 
+ * @author Moshe
+ */
+public class FrameDump {
+
+	public FrameDump() {
+		super();
+	}
+
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 *            the array of arguments.
+	 * @throws IOException
+	 *             if an I/O error occurs.
+	 */
+	public static void main(String[] args) throws IOException {
+		File file = new File(args[0]);
+		FrameDump dumper = new FrameDump();
+		dumper.dump(file);
+	}
+
+	public void dump(File file) throws IOException {
+		InputStream in = null;
+		NSOFDecoder decoder = new NSOFDecoder();
+		NSOFFrame frame;
+		try {
+			in = new FileInputStream(file);
+			frame = (NSOFFrame) decoder.inflate(in);
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+
+		dump(frame);
+	}
+
+	protected void dump(NSOFFrame frame) {
+		StringBuilder buf = new StringBuilder();
+		dump(frame, buf);
+		System.out.println(buf);
+	}
+
+	protected void dump(NSOFFrame frame, StringBuilder buf) {
+		buf.append('{').append('\n');
+
+		NSOFObject value;
+		int i = 0;
+		for (NSOFSymbol key : frame.getKeys()) {
+			if (i > 0)
+				buf.append(",\n");
+			value = frame.get(key);
+			buf.append(key.getValue());
+			buf.append('=');
+			if (value instanceof NSOFFrame) {
+				dump((NSOFFrame) value, buf);
+			} else {
+				buf.append(value.toString());
+			}
+			i++;
+		}
+		buf.append('\n').append('}');
+	}
+}
